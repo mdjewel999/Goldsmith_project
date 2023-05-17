6099a4e6d581559f84acb2bdefc24499495aae4c
mdjewel999 <mdjewel878761@gmail.com>
2023-05-18 02:33:21 +0600

    slider code added

---
+++ src/Pages/Home/Home/Services/Services.jsx
@@ -0,0 +1,73 @@
+// import React from 'react';
+
+const Services = () => {
+  return (
+      <>
+        <h1 className="text-center mt-8 font-bold text-5xl">Latest Products</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 mt-8 mb-8">
+      <div className="card w-52 bg-base-100 shadow-md">
+        <figure>
+          <img
+            src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+            alt="Shoes"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+      <div className="card w-52 bg-base-100 shadow-md">
+        <figure>
+          <img
+            src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+            alt="Shoes"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+      <div className="card w-52 bg-base-100 shadow-md">
+        <figure>
+          <img
+            src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+            alt="Shoes"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+      <div className="card w-52 bg-base-100 shadow-md">
+        <figure>
+          <img
+            src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+            alt="Shoes"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+    </div>
+      </>
+  );
+};
+
+export default Services;
--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -5,34 +5,34 @@ import './Footer.css'
 const Footer = () => {
   return (
     <div className='footer'>
-        <footer className="footer p-10  text-base-content">
+        <footer className="footer p-10  text-base-content h-auto">
       <div>
        <img className='logo_img' src={logo} alt="" />
-        <p>
+        <p className='text-white font-bold'>
           ACME Industries Ltd.
           <br />
           Providing reliable tech since 1992
         </p>
       </div>
       <div>
-        <span className="footer-title">Services</span>
-        <a className="link link-hover">Branding</a>
-        <a className="link link-hover">Design</a>
-        <a className="link link-hover">Marketing</a>
-        <a className="link link-hover">Advertisement</a>
+        <span className="footer-title text-white font-bold">Services</span>
+        <a className="link link-hover text-white font-bold">Branding</a>
+        <a className="link link-hover text-white font-bold">Design</a>
+        <a className="link link-hover text-white font-bold">Marketing</a>
+        <a className="link link-hover text-white font-bold">Advertisement</a>
       </div>
       <div>
-        <span className="footer-title">Company</span>
-        <a className="link link-hover">About us</a>
-        <a className="link link-hover">Contact</a>
-        <a className="link link-hover">Jobs</a>
-        <a className="link link-hover">Press kit</a>
+        <span className="footer-title text-white font-bold">Company</span>
+        <a className="link link-hover text-white font-bold">About us</a>
+        <a className="link link-hover text-white font-bold">Contact</a>
+        <a className="link link-hover text-white font-bold">Jobs</a>
+        <a className="link link-hover text-white font-bold">Press kit</a>
       </div>
       <div>
-        <span className="footer-title">Legal</span>
-        <a className="link link-hover">Terms of use</a>
-        <a className="link link-hover">Privacy policy</a>
-        <a className="link link-hover">Cookie policy</a>
+        <span className="footer-title text-white font-bold">Legal</span>
+        <a className="link link-hover text-white font-bold">Terms of use</a>
+        <a className="link link-hover text-white font-bold">Privacy policy</a>
+        <a className="link link-hover text-white font-bold">Cookie policy</a>
       </div>
     </footer>
     </div>

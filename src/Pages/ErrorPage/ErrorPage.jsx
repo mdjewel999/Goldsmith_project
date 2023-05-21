33a3caba09cfd71dd0eda2ca5b5e34b98defbfa4
mdjewel999 <mdjewel878761@gmail.com>
2023-05-21 20:54:51 +0600

    Navbar code and style chang

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -7,7 +7,7 @@ import img404 from "../../assets/images/services/404.jpg";
 const ErrorPage = () => {
     return (
         <div>
-      <img src={img404} alt="Error" />
+      <img className="error_img rounded" src={img404} alt="Error" />
       <Link className="go_to_home " to="/"><button className="btn btn-active btn-secondary ">Go to Home Page</button></Link>
     </div>
     );

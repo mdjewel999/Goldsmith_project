5bba766ee1a1e547b49d40188209477e6466b556
mdjewel999 <mdjewel878761@gmail.com>
2023-05-21 21:04:10 +0600

    error page animation and update code

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,16 +1,17 @@
 // import React from 'react';
-
-import { Link } from "react-router-dom";
-import './ErrorPage.css'
-import img404 from "../../assets/images/services/404.jpg";
+import { Link } from 'react-router-dom';
+import './ErrorPage.css';
+import img404 from '../../assets/images/services/404.jpg';
 
 const ErrorPage = () => {
-    return (
-        <div>
-      <img className="error_img rounded" src={img404} alt="Error" />
-      <Link className="go_to_home " to="/"><button className="btn btn-active btn-secondary ">Go to Home Page</button></Link>
+  return (
+    <div className="error-page">
+      <img className="error-img" src={img404} alt="Error" />
+      <Link className="go-to-home" to="/">
+        <button className="btn btn-active btn-secondary">Go to Home Page</button>
+      </Link>
     </div>
-    );
+  );
 };
 
 export default ErrorPage;

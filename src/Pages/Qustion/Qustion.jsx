d622ab75e4da092e673c8f7c531cc6e68c0dcc65
mdjewel999 <mdjewel878761@gmail.com>
2023-10-08 18:19:31 +0600

    blog sit code added

--- src/Pages/Qustion/Qustion.jsx
+++ src/Pages/Qustion/Qustion.jsx
@@ -8,124 +8,56 @@ const Qustion = () => {
       <div className="bg-base-200">
         <div>
           <h1 className="text-3xl font-bold">
-            1. What is an access token and refresh token?
+          ********************User Authentication:*******************
           </h1>
           <p className="py-6">
-            An access token is a credential that is used to authenticate and
-            authorize access to protected resources in an application or system.
-            It is typically a string of characters that is issued by an
-            authentication server after a successful authentication process. The
-            access token is included in each request to the server and is used
-            to verify the user identity and permissions. It grants temporary
-            access to specific resources for a limited period of time. A refresh
-            token, on the other hand, is a credential that is used to obtain a
-            new access token after the current one expires. Refresh tokens are
-            typically issued along with the access token by the authentication
-            server. When the access token expires, the refresh token can be used
-            to request a new access token without requiring the user to re-enter
-            their credentials. This helps improve the user experience by
-            allowing seamless and continuous access to protected resources. In
-            summary, an access token is used to authenticate and authorize
-            access to resources, while a refresh token is used to obtain a new
-            access token when the current one expires.
+          Implement a secure user authentication system that allows users to register and log in to your website.
+Users can register with their name, email, password, and optionally provide additional information like a profile picture, gender, phone number, and address.
+Provide password security measures such as password confirmation and the ability to hide/unhide the password.
           </p>
         </div>
         <div>
           <h1 className="text-3xl font-bold">
-            2. How do they work and where should we store them on the
-            client-side?
+          **********************Jewelry Catalog:********************
           </h1>
           <p className="py-6">
-            Access tokens and refresh tokens work together to provide secure and
-            efficient authentication and authorization. Here a short answer to
-            your question: Access tokens are typically stored on the
-            client-side, often in memory or local storage, as they are needed
-            for each request to the server to authenticate and authorize access
-            to protected resources. Refresh tokens, on the other hand, should be
-            stored securely on the client-side, such as in an HTTP-only cookie
-            or a secure storage mechanism, to prevent unauthorized access. They
-            are used to obtain a new access token when the current one expires,
-            without requiring the user to re-authenticate. It is important to
-            follow security best practices when handling and storing tokens on
-            the client-side, such as using secure storage mechanisms and
-            protecting against cross-site scripting (XSS) attacks.
+          Create a visually appealing catalog of jewelry items that users can browse.
+Display jewelry items with images, titles, prices, and brief descriptions.
+Implement categories and filters for easy jewelry navigation.
+Allow users to click on jewelry items to view detailed product information.
           </p>
         </div>
         <div>
           <h1 className="text-3xl font-bold">
-            3. Compare SQL and NoSQL databases?
+          ***********************User Profile:******************
           </h1>
           <p className="py-6">
-            <span className="text-3xl">SQL Databases:</span> <br />
-            Structured Query Language (SQL) databases are based on a relational
-            model. They use a structured schema to define tables, columns, and
-            relationships between entities. SQL databases ensure ACID
-            (Atomicity, Consistency, Isolation, Durability) properties,
-            providing strong consistency and data integrity. They are suitable
-            for complex queries and transactions. Examples: MySQL, PostgreSQL,
-            Oracle. <br />
-            <span className="text-3xl">NoSQL Databases:</span> <br />
-            NoSQL databases are non-relational and provide flexible data models.
-            They can handle unstructured, semi-structured, and structured data.
-            NoSQL databases offer horizontal scalability and high performance
-            for large-scale distributed systems. They trade off some consistency
-            for scalability and availability, often providing eventual
-            consistency. They are suitable for handling large amounts of data,
-            real-time applications, and rapid development. Examples: MongoDB,
-            Cassandra, Redis.
+           
+Provide registered users with a profile section where they can update their personal information, including name, photo, gender, phone number, and address.
+Users can view their purchase history or saved items if applicable.
+Display the users profile picture in the websits navbar.
           </p>
         </div>
         <div>
           <h1 className="text-3xl font-bold">
-            4. What is express js? What is Nest JS ?
+          *****************************Blog Section:***********************
           </h1>
           <p className="py-6">
-            <span className="text-3xl"> Express.js:</span>
-            Express.js is a minimalistic and flexible web application framework
-            for Node.js. It provides a set of robust features for building web
-            applications and APIs. Express.js simplifies the process of handling
-            HTTP requests, routing, middleware, and response handling. It allows
-            developers to create server-side applications efficiently and
-            quickly. Express.js has a large and active community, along with
-            numerous third-party libraries and middleware. It is widely used for
-            building lightweight, scalable, and modular web applications. <br />
-            <span className="text-3xl"> Nest.js:</span> <br />
-            Nest.js is a progressive and opinionated Node.js framework for
-            building efficient, scalable, and maintainable server-side
-            applications. It is built on top of Express.js and enhances it with
-            additional features and architectural patterns. Nest.js follows the
-            modular design principle, promoting the use of decorators, modules,
-            services, and controllers. It provides out-of-the-box support for
-            features like dependency injection, middleware, exception handling,
-            and more. Nest.js is designed to support the development of
-            enterprise-grade applications with a focus on code organization and
-            scalability. It is gaining popularity for its strong typing and
-            TypeScript integration.
+          Include a blog section where you can regularly publish articles related to jewelry, fashion, or related topics.
+Users can read and comment on blog posts.
+Implement a user-friendly interface for blog browsing and searching.
           </p>
         </div>
         <div>
           <h1 className="text-3xl font-bold">
-            5. What is MongoDB aggregate and how does it work ?
+          *************************Responsive Design and Navigation:********************
           </h1>
           <p className="py-6">
-            <span className="text-3xl">MongoDB Aggregation:</span>
-            MongoDB aggregation framework is a powerful tool for processing and
-            transforming data within the database. It allows you to perform
-            complex data manipulations, aggregations, and computations on the
-            documents stored in MongoDB collections. Aggregation operations in
-            MongoDB are performed using a pipeline approach, where multiple
-            stages are combined to process the data. Each stage in the pipeline
-            performs a specific operation on the documents, such as filtering,
-            grouping, sorting, projecting, and transforming the data. The
-            pipeline stages are executed sequentially, with the output of one
-            stage serving as the input for the next stage. Aggregation pipelines
-            can be constructed using a combination of various stages to achieve
-            the desired data transformation and analysis. The aggregation
-            framework supports a wide range of operators and expressions for
-            performing operations like matching, grouping, sorting, joining,
-            calculating aggregates, and more. It provides a flexible and
-            efficient way to process large volumes of data directly within the
-            database.
+          
+Ensure that your website has a responsive design that adapts to various screen sizes and devices.
+Implement a user-friendly navigation menu in the websites navbar with links to Home, All Jewelry, My Jewelry, Add Jewelry, Blogs, and the user's profile.
+Keep the navbar and a stylish footer consistent across all pages except the 404 error page.
+Design the website with a visually appealing color scheme, proper alignment, and adequate spacing to enhance the user experience.
           </p>
         </div>
         <div className="text-center mt-4 mb-8">
